8b78e22f6546bb139e5f25ef8cfa74945612e444
isai7710 <isai10sanchez@gmail.com>
2024-05-22 18:24:23 -0600

    added configuration files that I had before from my raspberry pi config and added some small changes

---
+++ init.lua
@@ -0,0 +1,52 @@
+--[[ 
+-- Welcome esteemed traveler to my personal neovim config
+-- couldn't have started this config withou help from the kickstart.nvim and modular-kickstart.nvim repos, shout out TJ fr
+
+10-15 min Lua guide:
+  - https://learnxinyminutes.com/docs/lua/
+
+Use `:help lua-guide` as a reference for how Neovim integrates Lua.
+- :help lua-guide
+- (or HTML version): https://neovim.io/doc/user/lua-guide.html
+
+Kickstart Guide:
+  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
+
+  Next, run AND READ `:help`.
+    This will open up a help window with some basic information
+    about reading, navigating and searching the builtin help documentation.
+
+    This should be the first place you go to look when you're stuck or confused
+    with something. It's one of my favorite Neovim features.
+
+    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
+    which is very useful when you're not exactly sure of what you're looking for.
+
+If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
+--]]
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true 
+
+-- NOTE: whenever we write ' require 'module' ' Neovim will look in the ~/.config/nvim/lua folder for the specific module/folder with submodules
+
+-- [[ Setting options ]]
+require 'options'
+
+-- [[ Basic Keymaps ]]
+require 'keymaps'
+
+-- [[ Basic Autocommands ]]
+require 'autocmds'
+
+-- [[ Bootstrap `lazy.nvim` plugin manager and install/configure plugins ]]
+require 'lazy-config'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/lazy-config.lua
@@ -0,0 +1,71 @@
+--[[ Bootstrap the lazy.nvim plugin manager ]]
+--  NOTE: See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+--[[ Install plugins using lazy ]]
+--  NOTE: To check the current status of your plugins, run the following 
+--    :Lazy
+--  In this menu you can press '?' for help or use `:q` to close the window
+--  To update plugins you can run
+--    :Lazy update
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+
+  -- NOTE: Plugins can also be added by using a table,
+  -- with the first argument being the link and the following
+  -- keys can be used to configure plugin behavior/loading/etc.
+  --
+  -- Use `opts = {}` to force a plugin to be loaded.
+  --
+  --  This is equivalent to:
+  --    require('Comment').setup({})
+
+require('lazy').setup({
+  require 'plugins/init',
+  require 'plugins/colorscheme',
+  require 'plugins/gitsigns',
+  require 'plugins/which-key',
+  require 'plugins/telescope', 
+  -- TODO: figure out how to configure plugins below so that they are optimized for rasp pi 2
+  -- require 'plugins/lspconfig',
+  -- require 'plugins/conform',     (autoformat)
+  -- require 'plugins/cmp',         (autocompletion)
+  require 'plugins/todo-comments',
+  require 'plugins/mini',
+  require 'plugins/treesitter',
+  require 'plugins/nvim-tree',
+
+  --  Here are some example plugins that I've included in the Kickstart repository.
+  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
+  --
+  -- require 'kickstart.plugins.debug',
+  require 'plugins/indent-line'
+  -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
