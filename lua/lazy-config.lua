e4cc805c3e57fc895fcf2e0c1e1aa340762190a9
isai7710 <isai10sanchez@gmail.com>
2024-05-28 12:13:02 -0600

    added nvim tree, figuring out configs and such

--- lua/lazy-config.lua
+++ lua/lazy-config.lua
@@ -33,11 +33,11 @@ require('lazy').setup({
   require 'plugins/todo-comments',
   require 'plugins/mini',
   require 'plugins/treesitter',
+  require 'plugins/nvim-tree',
   -- TODO: figure out how to configure plugins below 
   -- require 'plugins/lspconfig',
   -- require 'plugins/conform',     (autoformat)
   -- require 'plugins/cmp',         (autocompletion)
-  -- require 'plugins/nvim-tree',
 
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-tree/nvim-tree.lua',
+  lazy = false,
   dependencies = {
     'nvim-tree/nvim-web-devicons'
   },
@@ -46,19 +47,15 @@ return {
       },
     })
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+    -- toggle file explorer:
+    vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    -- toggle file explorer on current file:
+    vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) 
+    -- collapse file explorer:
+    vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    -- refresh file explorer:
+    vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
 
-    -- toggle file explorer
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-    -- toggle file explorer on current file
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { 
-      desc = "Toggle file explorer on current file" 
-    }) 
-    -- collapse file explorer
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
-    -- refresh file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
