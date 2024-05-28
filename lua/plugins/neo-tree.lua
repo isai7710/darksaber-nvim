860f5967eb0095b065530f7bf67a5bee54c07ef7
isai7710 <isai10sanchez@gmail.com>
2024-05-28 13:28:18 -0600

    trying to figure out neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  --[[
+  opts = {
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          modified = 'x',
+        }
+      }
+    }
+  },
+  --]]
+  config = function()
+    vim.keymap.set('n', '<leader>fe', ':Neotree filesystem reveal left<CR>', {desc = 'Show neo-tree file explorer on left'})
+  end
+}
