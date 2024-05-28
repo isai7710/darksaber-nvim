afebf6a73afdf3fecefbf05ff869e3765f043b68
isai7710 <isai10sanchez@gmail.com>
2024-05-28 12:51:13 -0600

    better modularization of configuration now, going to try neotree next

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 --[[ 
 -- Welcome esteemed traveler to my personal neovim config
--- couldn't have started this config withou help from the kickstart.nvim and modular-kickstart.nvim repos, shout out TJ fr, I left a lot of helpful comments from those repositories here for help
+-- I couldn't have started this config withou help from the kickstart.nvim and modular-kickstart.nvim repos, shout out TJ fr, I left a lot of helpful comments from those repositories here for help
 
 10-15 min Lua guide:
   - https://learnxinyminutes.com/docs/lua/
@@ -16,13 +16,10 @@ Kickstart Guide:
     This will open up a help window with some basic information
     about reading, navigating and searching the builtin help documentation.
 
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
     MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
     which is very useful when you're not exactly sure of what you're looking for.
 
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
+run `:checkhealth` for more info on Neovim health
 --]]
 
 -- Set <space> as the leader key
@@ -34,7 +31,7 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true 
 
--- NOTE: whenever we write 
+-- NOTE: whenever we write...
 --    require 'module-name' 
 -- in this root init.lua file, Neovim will look in the ~/.config/nvim/lua folder for the 
 -- specific module-name, which can be a lua module itself OR a folder with lua submodules
@@ -48,8 +45,39 @@ require 'keymaps'
 -- [[ Basic Autocommands ]]
 require 'autocmds'
 
--- [[ Bootstrap the `lazy.nvim` plugin manager and install/configure plugins ]]
-require 'lazy-config'
+--[[ Bootstrap the lazy.nvim plugin manager ]]
+--  NOTE: See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system { 
+    'git', 
+    'clone', 
+    '--filter=blob:none', 
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', 
+    lazypath 
+  }
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+--[[ Install plugins using lazy ]]
+require('lazy').setup('plugins')
+
+--  NOTE: To check the current status of your plugins, run the following 
+--    :Lazy
+--
+-- TODO: figure out how to configure plugins below 
+-- require 'plugins/lspconfig',
+-- require 'plugins/conform',     (autoformat)
+-- require 'plugins/cmp',         (autocompletion)
+--
+-- NOTE: Kickstart plugins you may want to take a look at later:
+-- require 'kickstart.plugins.debug',
+-- require 'kickstart.plugins.lint',
+-- require 'kickstart.plugins.autopairs',
+-- require 'kickstart.plugins.neo-tree',
+-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
