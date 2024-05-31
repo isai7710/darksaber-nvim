eed49b45b2b881705de45ff8be73ab0ec07dc6ad
isai7710 <isai10sanchez@gmail.com>
2024-05-30 20:06:20 -0600

    configured linting by adding autocmd and keymaps

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -3,10 +3,26 @@ return {
   -- similar to the formatter plugin conform.lua, we want to load this plugin on the following events:
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    require('lint').linters_by_ft = {
+    -- load the plugin
+    local lint = require('lint')
+    -- specify linters by filetype, ensure these were installed by the mason-tool-installer
+    -- which is found as a dependency of nvim-lspconfig in the lsp-config.lua file OR manually
+    -- install them (through the Mason UI or other means)
+    lint.linters_by_ft = {
       javascript = { 'eslint_d' },
       javascriptreact = { 'eslint_d' },
       python = { 'pylint' },
     }
+
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      group = vim.api.nvim_create_augroup('lint', { clear = true }),
+      callback = function()
+        lint.try_lint()
+      end
+    })
+
+    vim.keymap.set('n', '<leader>l', function()
+      lint.try_lint()
+    end, { desc = 'Trigger [L]inting for current file' })
   end
 }
