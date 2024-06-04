a1d31498c52a362eea73eda48871c7aec11c0cac
isai7710 <isai10sanchez@gmail.com>
2024-06-04 12:35:12 -0600

    small component visibility changes for lualine bar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,11 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
-      theme = 'dracula'
+      theme = 'catppuccin'
+    },
+    sections = {
+      lualine_x = { 'filetype' },
+      lualine_y = { "os.date('%a %m/%d')" },
     }
   }
 }
