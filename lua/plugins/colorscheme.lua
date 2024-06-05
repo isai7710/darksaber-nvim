815431d29d6eef71366039a6c6ff3cf4ab3eaebd
isai7710 <isai10sanchez@gmail.com>
2024-06-05 14:41:01 -0600

    somethings up with catppuccin or my terminal lol

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,26 @@
 return {
-  "catppuccin/nvim",
-  opts = {
-    flavour = 'mocha',
-    styles = {
-      comments = {},
-    },
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      treesitter = true,
-      mason = true,
-      telescope = {
-        enabled = true,
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      term_colors = true,
+      flavour = 'mocha',
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        mason = true,
+        telescope = {
+          enabled = false,
+        },
+        which_key = false,
+        neotree = true,
       },
-      which_key = false,
-      neotree = true,
     },
+    init = function()
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end
   },
-  name = "catppuccin",
-  priority = 1000,
-  init = function()
-    vim.cmd.colorscheme 'catppuccin'
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-  end,
 }
