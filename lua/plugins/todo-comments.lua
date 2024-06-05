6a631526a80847c4ab75dcca1ea3e2a584c9a288
isai7710 <isai10sanchez@gmail.com>
2024-06-05 15:27:16 -0600

    indentation fixes

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,8 +1,8 @@
 return {
   -- Highlight todo, notes, etc in comments
-  'folke/todo-comments.nvim', 
-  dependencies = { 'nvim-lua/plenary.nvim' }, 
-  opts = { 
+  'folke/todo-comments.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
     signs = false,
   }
 }
