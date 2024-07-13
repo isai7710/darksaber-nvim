0d522c679a12b7780c243a056347af6e21d9e633
isai7710 <isai10sanchez@gmail.com>
2024-07-13 14:22:10 -0600

    updated plugins in lazy, stuff broke, learned lesson on the importance of lockfile for backups (useful to fall back on when I don't want to deal with stuff breaking and go back to a working set up) and now my lazy-lock.json file should be version controled in git whenever I need to revert to it

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -14,14 +14,14 @@
 --  config = function() ... end
 
 return {
-  { -- Useful plugin to show you pending keybinds.
+  {                     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
+      require('which-key').register({
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
@@ -29,7 +29,7 @@ return {
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
+      })
       -- visual mode
       require('which-key').register({
         ['<leader>h'] = { 'Git [H]unk' },
