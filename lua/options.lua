e0f75d3b7f75a9400a45e7b20a85ae2ccbce2716
isai7710 <isai10sanchez@gmail.com>
2024-07-13 13:27:51 -0600

    small edits

--- lua/options.lua
+++ lua/options.lua
@@ -16,6 +16,7 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
+
 -- The following allow us to yank not only within Neovim but now anything we yank is automatically added to OS clipboard
 -- Similarly with pasting, anything copied on OS clipboard can be pasted on Neovim with "p"
 --  there was a lot of trial and error with this (using ChatGPT and Neovim docs) so I'm still not completely sure how it works
