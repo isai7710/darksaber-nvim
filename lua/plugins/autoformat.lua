a57c72977f55a6d3947fa46b586541e8a83b4563
isai7710 <isai10sanchez@gmail.com>
2024-08-29 14:24:54 -0600

    restructured for more uniform plugin loading across configuration

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -1,50 +1,48 @@
 return {
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    -- lazy load on the following events (we only need formatting when we're working inside a buffer)
-    -- BufReadPre: when we open an already existing file
-    -- BufNewFile: triggered when we open a buffer for a file that doesn't already exist
-    event = { "BufReadPre", "BufNewFile" },
-    -- set the following keymap to autoformat
-    keys = {
-      {
-        '<leader>af',
-        function()
-          require('conform').format({
-            async = true,
-            lsp_fallback = true
-          })
-        end,
-        mode = { 'n', 'v' },
-        desc = '[A]uto[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
+  'stevearc/conform.nvim',
+  -- lazy load the autoformat plugin on the following events (we only need formatting when we're working inside a buffer)
+  -- BufReadPre: when we open an already existing file
+  -- BufNewFile: triggered when we open a buffer for a file that doesn't already exist
+  event = { "BufReadPre", "BufNewFile" },
+  -- set the following keymap to autoformat
+  keys = {
+    {
+      '<leader>af',
+      function()
+        require('conform').format({
+          async = true,
+          lsp_fallback = true
+        })
       end,
-      formatters_by_ft = {
-        -- lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        python = { "isort", "black" },
-        -- You can use a sublist to run only the first available formatter
-        javascript = { { "prettierd", "prettier" } },
-        javascriptreact = { "prettier" },
-        typescript = { { "prettierd", "prettier" } },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        markdown = { "prettier" },
-      },
+      mode = { 'n', 'v' },
+      desc = '[A]uto[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      return {
+        timeout_ms = 500,
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      }
+    end,
+    formatters_by_ft = {
+      -- lua = { 'stylua' },
+      -- Conform can also run multiple formatters sequentially
+      python = { "isort", "black" },
+      -- You can use a sublist to run only the first available formatter
+      javascript = { { "prettierd", "prettier" } },
+      javascriptreact = { "prettier" },
+      typescript = { { "prettierd", "prettier" } },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      markdown = { "prettier" },
     },
   },
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,35 +1,34 @@
 return {
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    opts = {
-      dim_inactive = {
-        enabled = true,
-        shade = 'dark',
-        percentage = 0.5,
-      },
-      term_colors = true,
-      flavour = 'mocha',
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        indent_blankline = {
-          enabled = true,
-          scope_color = "",
-          colored_indent_levels = false,
-        },
-        treesitter = true,
-        mason = true,
-        telescope = {
-          enabled = true,
-        },
-        which_key = true,
-        neotree = true,
-      },
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  opts = {
+    dim_inactive = {
+      enabled = true,
+      shade = 'dark',
+      percentage = 0.5,
     },
-    init = function()
-      vim.cmd.colorscheme 'catppuccin'
-    end
+    term_colors = true,
+    flavour = 'mocha',
+    integrations = {
+      bufferline = false,
+      cmp = true,
+      gitsigns = true,
+      indent_blankline = {
+        enabled = true,
+        scope_color = "",
+        colored_indent_levels = false,
+      },
+      treesitter = true,
+      mason = true,
+      telescope = {
+        enabled = true,
+      },
+      which_key = true,
+      neotree = true,
+    },
   },
+  init = function()
+    vim.cmd.colorscheme 'catppuccin'
+  end
 }
