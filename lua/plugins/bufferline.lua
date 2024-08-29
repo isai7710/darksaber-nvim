5e8d1b9be0b64e0fd05396f7227dd31918575069
isai7710 <isai10sanchez@gmail.com>
2024-08-29 14:48:25 -0600

    changed some styling, looks good now

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,22 +4,27 @@ return {
   version = "*",
   dependencies = {
     'nvim-tree/nvim-web-devicons',
-    -- the bufdelete plugin makes deleting buffers much less annoying, without this the window layout was shifty
-    -- upon default buffer deletion command (:bd), now if you execute :Bd the window won't shift and next buffer opens automatically
+    -- the bufdelete plugin makes deleting buffers much less annoying with the provided :Bdelete command, default buffer deletion was :bd
     'famiu/bufdelete.nvim',
   },
   config = function()
     vim.opt.termguicolors = true
+
+    -- a couple of keymaps to cycle through, delete, reorder, and pin buffers
     vim.keymap.set('n', '[b', ':bp<CR>', { desc = 'Go to previous buffer' })
     vim.keymap.set('n', ']b', ':bp<CR>', { desc = 'Go to next buffer' })
     vim.keymap.set('n', '<leader>bd', ':Bdelete<CR>', { desc = 'Delete current buffer without shifting window layout' })
     vim.keymap.set('n', '<leader>bl', ':BufferLineMoveNext', { desc = 'Reorder current buffer right' })
     vim.keymap.set('n', '<leader>bh', ':BufferLineMovePrev', { desc = 'Reorder current buffer left' })
     vim.keymap.set('n', '<leader>bp', ':BufferLineTogglePin', { desc = 'Pin current buffer' })
-    require('bufferline').setup({
+
+    local bufferline = require('bufferline')
+    bufferline.setup({
       options = {
         diagnostics = "nvim_lsp",
-        separator_style = "slant",
+        separator_style = "thick",
+        show_close_icon = false,
+        show_tab_indicators = true,
         offsets = {
           {
             filetype = "neo-tree",
