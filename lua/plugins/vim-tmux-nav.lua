25530d2a1f85a5e29a95ae6e7b46b75a8369ece8
isai7710 <isai10sanchez@gmail.com>
2024-08-30 01:34:30 -0600

    changed <C-\> remap so there's no conflict with opening toggleterm (which uses <C-\> by default which I like)

--- lua/plugins/vim-tmux-nav.lua
+++ lua/plugins/vim-tmux-nav.lua
@@ -1,12 +1,20 @@
---[[
-This plugin provides the following mappings which allow you to move between Vim panes and tmux splits seamlessly.
-<ctrl-h> => Left
-<ctrl-j> => Down
-<ctrl-k> => Up
-<ctrl-l> => Right
-<ctrl-\> => Previous split
-Note - you don't need to use your tmux prefix key sequence before using the mappings.
---]]
+-- This plugin provides the following mappings which allow you to move between Vim panes and tmux splits seamlessly.
+-- Note - you don't need to use your tmux prefix key sequence before using the mappings.
 return {
-  'christoomey/vim-tmux-navigator'
+  'christoomey/vim-tmux-navigator',
+  -- Lazy-load the plugin on the following commands and keymaps
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-p>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
 }
