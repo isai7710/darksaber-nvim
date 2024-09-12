06f93e37ba40d286147c47c485f975b25ad4c1f7
isai7710 <isai10sanchez@gmail.com>
2024-09-12 13:02:26 -0600

    removed unecessary comments

--- init.lua
+++ init.lua
@@ -48,17 +48,20 @@ require 'autocmds'
 --[[ Bootstrap the lazy.nvim plugin manager ]]
 --  NOTE: See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath
-  }
-end ---@diagnostic disable-next-line: undefined-field
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
 vim.opt.rtp:prepend(lazypath)
 
 --[[ Install plugins using lazy ]]
@@ -66,13 +69,6 @@ require('lazy').setup('plugins')
 
 --  NOTE: To check the current status of your plugins, run the following
 --    :Lazy
---
--- NOTE: Kickstart plugins you may want to take a look at later:
--- require 'kickstart.plugins.debug',
--- require 'kickstart.plugins.lint',
--- require 'kickstart.plugins.autopairs',
--- require 'kickstart.plugins.neo-tree',
--- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
