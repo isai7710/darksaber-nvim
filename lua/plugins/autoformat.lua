f1ab66a7313d8418e1eb81fa77cf76b076ce2f3c
isai7710 <isai10sanchez@gmail.com>
2024-09-12 12:56:47 -0600

    removed some deprecated features and updated conform based on kickstart.nvim's changes in c76c323 commit

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -11,7 +11,7 @@ return {
       function()
         require('conform').format({
           async = true,
-          lsp_fallback = true
+          lsp_format = "fallback",
         })
       end,
       mode = { 'n', 'v' },
@@ -24,10 +24,17 @@ return {
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
+      -- local disable_filetypes = { c = true, cpp = true }
+      local disable_filetypes = {}
+      local lsp_format_opt
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        lsp_format_opt = 'never'
+      else
+        lsp_format_opt = 'fallback'
+      end
       return {
         timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        lsp_format = lsp_format_opt
       }
     end,
     formatters_by_ft = {
