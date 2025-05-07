d0dd5790e3efd74dab1e14b2eff2309aebfd1b61
Isaito <isaisanchezcc@gmail.com>
2025-05-07 13:36:54 -0600

    small customization changes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,11 +4,15 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
-      theme = 'catppuccin'
+      section_separators = { left = '', right = '' },
+      theme = 'catppuccin',
+      globalstatus = true,
     },
     sections = {
-      lualine_x = { 'filetype' },
+      lualine_c = { "filename" },
+      lualine_x = { "filetype" },
       lualine_y = { "os.date('%a %m/%d')" },
+      lualine_z = { "lsp_status" },
     }
   }
 }
