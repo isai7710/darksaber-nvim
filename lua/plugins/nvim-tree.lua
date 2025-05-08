f3c5783114f0a0aeaaccedba7a0c1324919d9b76
Isaito <isaisanchezcc@gmail.com>
2025-05-08 13:56:40 -0600

    customizing changes

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,9 +8,34 @@ return {
   config = function()
     require("nvim-tree").setup({
       renderer = {
+        hidden_display = "simple",
+        highlight_git = "name",
+        indent_markers = {
+          enable = true,
+        },
         icons = {
-          git_placement = "after"
+          git_placement = "after",
+          modified_placement = "right_align",
+          show = {
+            folder_arrow = false,
+          },
         }
+      },
+      diagnostics = {
+        enable = true,
+        icons = {
+          hint = "󰌶",
+          info = "󰋽",
+          warning = "󰀪",
+          error = "󰅚"
+        }
+      },
+      modified = {
+        enable = true,
+      },
+      git = {
+        show_on_dirs = true,
+        show_on_open_dirs = false,
       }
     })
     vim.keymap.set('n', '<leader>fe', ':NvimTreeOpen <CR>', { desc = 'Show neo-tree file explorer ' })
