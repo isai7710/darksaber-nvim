b5b1148aa639b8ff344c0d9889d42a310a581104
Isaito <isaisanchezcc@gmail.com>
2025-05-08 12:30:01 -0600

    diagnostics config moved from here to lsp-config module

--- lua/options.lua
+++ lua/options.lua
@@ -83,46 +83,6 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
--- Diagnostic Config
--- See :help vim.diagnostic.Opts
-vim.diagnostic.config {
-  severity_sort = true,
-  float = { border = 'rounded', source = 'if_many' },
-  underline = { severity = vim.diagnostic.severity.ERROR },
-  signs = vim.g.have_nerd_font and {
-    text = {
-      [vim.diagnostic.severity.ERROR] = '󰅚 ',
-      [vim.diagnostic.severity.WARN] = '󰀪 ',
-      [vim.diagnostic.severity.HINT] = '󰌶 ',
-      [vim.diagnostic.severity.INFO] = '󰋽 ',
-    },
-  } or {},
-  virtual_text = {
-    source = 'if_many',
-    spacing = 2,
-    format = function(diagnostic)
-      local diagnostic_message = {
-        [vim.diagnostic.severity.ERROR] = diagnostic.message,
-        [vim.diagnostic.severity.WARN] = diagnostic.message,
-        [vim.diagnostic.severity.INFO] = diagnostic.message,
-        [vim.diagnostic.severity.HINT] = diagnostic.message,
-      }
-      return diagnostic_message[diagnostic.severity]
-    end,
-  },
-}
-
--- Show source in diagnostics
-vim.diagnostic.config({
-  virtual_text = {
-    source = "if_many",
-  },
-  float = {
-    source = "if_many",
-    border = "rounded",
-  },
-})
-
 vim.filetype.add({
   extension = {
     mdx = "markdown"
